b8bac1465cf76a30cad2b6f8e4952487544e8131
NotNatural21 <notnatural21@gmail.com>
2023-05-20 09:27:53 +1200

    Add course creation dialog
    Co-Authored-By: Giga <giga@digisomni.com>

---
+++ src/components/NewCourseDialog.vue
@@ -0,0 +1,68 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { Course } from '@/types';
+import firebase from '@/firebase';
+
+export default defineComponent({
+	name: 'NewSemesterDialog',
+	data () {
+		return {
+            firebase,
+            courseCode: '',
+            semIndex: 0
+		};
+	},
+	props: {
+		modelValue: { type: Boolean, default: false }
+	},
+	emits: ['update:modelValue'],
+	methods: {
+		cancel (): void {
+			this.$emit('update:modelValue', false);
+		},
+		save (): void {
+            firebase.dataBase.semesters[this.semIndex].courses.push({...new Course(this.courseCode)})
+            void firebase.updateDataBase();
+            this.semIndex = 0;
+            this.courseCode = '';
+			this.$emit('update:modelValue', false);
+		}
+	},
+	watch: {
+		modelValue (newValue) {
+			if (newValue) {
+				(this.$refs.dialog as HTMLDialogElement).showModal();
+			} else {
+				(this.$refs.dialog as HTMLDialogElement).close();
+			}
+		}
+	}
+});
+</script>
+
+<template>
+	<dialog ref="dialog">
+		<form @submit.prevent>
+			<fieldset>
+				<label for="newCourseName">
+					Course Code
+					<input type="text" id="newCourseName" v-model="courseCode">
+				</label>
+				<label for="newCourseSemester">
+					Semester
+					<select name="" id="newCourseSemester" v-model="semIndex">
+                        <option v-for="semester, index in firebase.dataBase.semesters" :key="index" :value="index">{{ new Date(semester.startDate).getFullYear() }} - {{ semester.name }}</option>
+                    </select>
+				</label>
+			</fieldset>
+			<fieldset>
+				<button @click="cancel()">Cancel</button>
+				<button @click="save()">Create</button>
+			</fieldset>
+		</form>
+	</dialog>
+</template>
+
+<style scoped>
+
+</style>
