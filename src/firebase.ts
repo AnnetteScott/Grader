27c636846451f99f05bcc66d8bf54c25226082c8
NotNatural21 <notnatural21@gmail.com>
2023-05-19 15:02:13 +1200

    Added Firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -10,8 +10,12 @@ import type { Firestore } from 'firebase/firestore';
 class Firebase {
 	static instance: Firebase;
 	private firebaseConfig = {
-		//ADD FIREBASE CONFIG
-        //TODO
+		apiKey: "AIzaSyBg2RJIEZNlj1N5EJzTKd8emeku_w-VOhM",
+		authDomain: "grader-dccee.firebaseapp.com",
+		projectId: "grader-dccee",
+		storageBucket: "grader-dccee.appspot.com",
+		messagingSenderId: "127382055466",
+		appId: "1:127382055466:web:fc8c843837f85a992a7153"
 	};
 	app: FirebaseApp;
 	auth: Auth;
@@ -19,50 +23,50 @@ class Firebase {
 	fireStoreUnsubscribe: Unsubscribe;
 	db: Firestore; // Firestore database
 	user = ref(null) as Ref<User | null>; //User
-    dataBase = reactive({}); // User data
+	dataBase = reactive({}); // User data
 
-    fireStorePath = '';
+	fireStorePath = '';
 
 	constructor () {
 		// Initialize Firebase.
 		this.app = initializeApp(this.firebaseConfig);
 
-        // Initialize Firestore.
+		// Initialize Firestore.
 		this.db = getFirestore(this.app)
 
-        this.fireStoreUnsubscribe = {} as Unsubscribe;
+		this.fireStoreUnsubscribe = {} as Unsubscribe;
 
 		// Initialize Firebase Authentication and get a reference to the service.
 		this.auth = getAuth(this.app);
 		this.authUnsubscribe = onAuthStateChanged(this.auth, (user) => {
 			this.auth = getAuth(this.app);
 			this.user.value = user;
-            
-            if(this.user.value){
-                this.fireStorePath = `${this.user.value.uid}/DashBooks`
+			
+			if(this.user.value){
+				this.fireStorePath = `${this.user.value.uid}/DashBooks`
 
-                try {
-                    this.fireStoreUnsubscribe();
-                } catch (error) {
-                    //console.warn(error)
-                }
+				try {
+					this.fireStoreUnsubscribe();
+				} catch (error) {
+					//console.warn(error)
+				}
 
-                this.fireStoreUnsubscribe = onSnapshot(doc(this.db, this.fireStorePath), async (doc) => {
-                    Object.assign(this.dataBase, doc.data())
-                    if(Object.keys(this.dataBase).length === 0){
-                        const newItem = {};
-                        await this.updateDataBase(newItem)
-                    }
-                    console.log(this.dataBase)
-                });
-            }else{
-                try {
-                    this.fireStoreUnsubscribe();
-                    Object.assign(this.dataBase, {})
-                } catch (error) {
-                    //console.warn(error)
-                }
-            }
+				this.fireStoreUnsubscribe = onSnapshot(doc(this.db, this.fireStorePath), async (doc) => {
+					Object.assign(this.dataBase, doc.data())
+					if(Object.keys(this.dataBase).length === 0){
+						const newItem = {};
+						await this.updateDataBase(newItem)
+					}
+					console.log(this.dataBase)
+				});
+			}else{
+				try {
+					this.fireStoreUnsubscribe();
+					Object.assign(this.dataBase, {})
+				} catch (error) {
+					//console.warn(error)
+				}
+			}
 		});
 		
 	}
@@ -77,14 +81,14 @@ class Firebase {
 		return Firebase.instance;
 	}
 
-    async updateDataBase(dataBase = this.dataBase, bypass = false){
-        if(this.user === null || !bypass){
-            return;
-        }
-        await setDoc(doc(this.db, this.fireStorePath), {
-            ...dataBase
-        })
-    }
+	async updateDataBase(dataBase = this.dataBase, bypass = false){
+		if(this.user === null || !bypass){
+			return;
+		}
+		await setDoc(doc(this.db, this.fireStorePath), {
+			...dataBase
+		})
+	}
 }
 
 export default Firebase.getInstance();
